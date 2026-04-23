a3343ea5e60eae38a0cea0949f37ff91612ef5ed
marlinarnz <marlin-arnz@web.de>
2026-04-23 15:07:23 +0200

    bugfix: remove metadata from Dashboard

--- streamlit/pages/1_Dashboard.py
+++ streamlit/pages/1_Dashboard.py
@@ -19,7 +19,6 @@ CURRENT_DIR = Path(__file__).parent.parent
 
 VERSION_FOR_DATA = "v28"
 VERSION_FOR_FITPARAMETERS = "v28"
-VERSION_FOR_METADATA = "v26"
 YEAR_PADDING_FOR_PLOTTING = 10
 PATH = "../data"
 
@@ -70,10 +69,6 @@ except FileNotFoundError:
     st.error(f"⚠️ Data version '{version_summary}' not found. Please enter a valid version. Otherwise, the default {VERSION_FOR_FITPARAMETERS} is used.")
     summary_df = load_summary(VERSION_FOR_FITPARAMETERS)
 
-# Load supplementary data
-fn_early = CURRENT_DIR / PATH / "EarlyAdopterRegions_perInnovation_21March.csv"  # Early Adopting regions
-fn_metadata = CURRENT_DIR / PATH / f"metadata_master_{VERSION_FOR_METADATA}.xlsx"
-fn_clusters = CURRENT_DIR / PATH / "innovation_list_HWLclusters_v3.0.xlsx"
 
 dosi_df["Value"] = pd.to_numeric(dosi_df["Value"], errors="coerce")
 dosi_df = dosi_df.dropna(subset=["Value"])
@@ -81,80 +76,9 @@ dosi_df = dosi_df.dropna(subset=["Value"])
 dosi_df["Spatial Scale"] = dosi_df["Spatial Scale"].str.rstrip()
 dosi_df["Innovation Name"] = dosi_df["Innovation Name"].str.rstrip()
 
-early_df = pd.read_csv(fn_early, usecols=[0, 1])
-early_dict = dict(zip(early_df.iloc[:, 0], early_df.iloc[:, 1]))
-
-
-# Metadata / codes
-
-def convert_to_three_digit_notation(s):
-    return re.sub(r"([a-zA-Z])(\d+)", lambda m: f"{m.group(1)}{int(m.group(2)):03}", s)
-
-
-def read_metadata_table(fn, columns):
-    df = pd.read_excel(fn, usecols=columns, dtype=str).dropna().reset_index(drop=True)
-    df.iloc[:, 1] = df.iloc[:, 1].apply(convert_to_three_digit_notation)
-    return df.set_index(df.columns[0])[df.columns[1]].to_dict()
-
-
-metadata = dict()
-metadata["Innovation Name"] = read_metadata_table(fn_metadata, "A,D")
-metadata["Spatial Scale"] = read_metadata_table(fn_metadata, "G,I")
-metadata["Indicator Name"] = read_metadata_table(fn_metadata, "L,M")
-metadata["Indicator Number"] = read_metadata_table(
-    fn_metadata, "L,O"
-)  # Column M is the indicator name. Superfluous because maps 1-1 on indicator number
-metadata["Description"] = read_metadata_table(fn_metadata, "R,S")
-metadata["Metric"] = read_metadata_table(fn_metadata, "V,W")
-
-for key, nested_dict in metadata.items():
-    if isinstance(nested_dict, dict):  # Ensure the value is a dictionary
-        metadata[key] = {
-            k.lower() if isinstance(k, str) else k: v for k, v in nested_dict.items()
-        }
-
-# Update metadata and early adopt innovation name label with cluster assignment file
-clusters = pd.read_excel(fn_clusters)
-clusters_names = clusters.set_index("innovation_name")["innovation_label"].to_dict()
-metadata["Innovation Name"].update(clusters_names)
-early_dict.update({code: 0 for code in clusters_names.values() if not code in early_dict.keys()})
-
-# Attach codes to data file
-
-dosi_df["Innovation Name"] = dosi_df["Innovation Name"].str.lower()
-dosi_df["Innovation Code"] = dosi_df["Innovation Name"].map(metadata["Innovation Name"])
-dosi_df["Region Code"] = (
-    dosi_df["Spatial Scale"].str.lower().map(metadata["Spatial Scale"])
-)
-dosi_df["Early Adopter Code"] = dosi_df["Innovation Code"].map(early_dict)
-dosi_df["Indicator Code"] = (
-    dosi_df["Indicator Number"].str.lower().map(metadata["Indicator Number"])
-)
-dosi_df["Description Code"] = (
-    dosi_df["Description"].str.lower().map(metadata["Description"])
-)
-dosi_df["Metric Code"] = dosi_df["Metric"].str.lower().map(metadata["Metric"])
-#dosi_df["Code"] = dosi_df[
-#    [
-#        "Innovation Code",
-#        "Region Code",
-#        "Indicator Code",
-#        "Description Code",
-#        "Metric Code",
-#    ]
-#].agg("_".join, axis=1)
-dosi_df["descriptionmetric"] = dosi_df[["Description", "Metric"]].agg(
-    " / ".join, axis=1
-)
-
-innovation_names = dosi_df["Innovation Name"].unique().tolist()
-innovation_names.sort()
-indicator_codes = dosi_df["Indicator Number"].unique().tolist()
-indicator_codes.sort()
-
-summary_df["Region Code"] = (
-    summary_df["Spatial Scale"].str.lower().map(metadata["Spatial Scale"])
-)
+innovation_names = sorted(dosi_df["Innovation Name"].unique().tolist())
+indicator_codes = summary_df["Indicator Number"].unique().tolist()
+indicator_names = summary_df["Indicator Name"].unique().tolist()
 
 
 def FPLogValue_with_scaling(x, t0, Dt, s):
@@ -170,71 +94,39 @@ col1, col2 = st.columns(2)
 with col1:
     selected_innovation = st.selectbox("Select innovation", innovation_names, index=0)
 
-early_adopting_region_code = early_dict[
-    metadata["Innovation Name"][selected_innovation]
-]
-
-other_region_codes = list(
-    map(metadata["Spatial Scale"].get,
-        [s.lower()
-         for s in dosi_df[dosi_df["Innovation Name"] == selected_innovation]["Spatial Scale"
-            ].unique().tolist()
-        ]))
-
-
-# function for reverse dictionary, taking the first element
-def rev_dict(dictionary: dict, label):
-    try:
-        return next(key for key, value in dictionary.items() if value == label)
-    except StopIteration:
-        return list(dictionary.keys())[0]
-
+# Filter data
+innovation_df = dosi_df[dosi_df["Innovation Name"] == selected_innovation].copy()
 
 with col2:
-
-    selected_spatial_scale_code = st.selectbox(
-        "Select spatial scale (first option is the early adopter region)",
-        [early_adopting_region_code] + other_region_codes,
-        # labels different from values
-        format_func=lambda x: rev_dict(metadata["Spatial Scale"], x),
-        # [rev_dict(metadata["Spatial Scale"], early_adopting_region_code)]
-        # + [rev_dict(metadata["Spatial Scale"], x) for x in other_region_codes],
+    selected_spatial_scale = st.selectbox(
+        "Select spatial scale",
+        list(innovation_df["Spatial Scale"].unique()),
         index=0,
     )
 
-innovation_df = dosi_df[
-    (
-        (dosi_df["Innovation Name"] == selected_innovation)
-        & (dosi_df["Region Code"] == selected_spatial_scale_code)
-    )
-].copy()
-
 
 NUMBER_OF_COLUMNS = 8  # Number of columns in the grid
-
 st.subheader("Indicators included:")
 cols = st.columns(NUMBER_OF_COLUMNS)
 feature_states = {}
-descriptionmetric_states = {}
-
 for idx, label in enumerate(indicator_codes):
     with cols[idx % NUMBER_OF_COLUMNS]:
-        descriptionmetric_states[label] = []
         feature_states[label] = st.checkbox(
-            label + " " + metadata["Indicator Name"][label],
+            label + " " + indicator_names[idx],
             value=label in innovation_df["Indicator Number"].unique().tolist(),
         )
 
 
+# Filter dataframes for selected innovation and spatial scale
 innovation_df_for_plotting = innovation_df[
-    innovation_df["Indicator Number"].isin([k for k, v in feature_states.items() if v])
+    (innovation_df["Indicator Number"].isin([k for k, v in feature_states.items() if v]))
+    & (innovation_df["Spatial Scale"]==selected_spatial_scale)
 ].copy()
 
-# Filter summary dataframe for selected innovation and spatial scale
 innovation_summary_df = summary_df[
     (
         (summary_df["Innovation Name"] == selected_innovation)
-        & (summary_df["Region Code"] == selected_spatial_scale_code)
+        & (summary_df["Spatial Scale"] == selected_spatial_scale)
     )
 ]
 
@@ -282,8 +174,8 @@ def build_plot(innovation_df, innovation_summary_df) -> go.Figure:
                     x=innovation_df[innovation_df["Indicator Number"] == code]["Year"],
                     y=innovation_df[innovation_df["Indicator Number"] == code]["Value"] / K,
                     mode="markers",
-                    name=f"""{code} {metadata['Indicator Name'][code]} (Metric {innovation_df[innovation_df["Indicator Number"] == code]["Metric"].unique().tolist()}) K-normalized data """,  # This can be the same name to link with the line in the legend
-                    hovertemplate=f"""{code} {metadata['Indicator Name'][code]} (Metric {innovation_df[innovation_df["Indicator Number"] == code]["Metric"].unique().tolist()}) <br>{code} Point<br>Year=%{{x:.0f}}<br>value=%{{y:.2f}}<extra></extra>""",  # Custom tooltip
+                    name=f"""{code} (Metric {innovation_df[innovation_df["Indicator Number"] == code]["Metric"].unique().tolist()}) K-normalized data """,  # This can be the same name to link with the line in the legend
+                    hovertemplate=f"""{code} (Metric {innovation_df[innovation_df["Indicator Number"] == code]["Metric"].unique().tolist()}) <br>{code} Point<br>Year=%{{x:.0f}}<br>value=%{{y:.2f}}<extra></extra>""",  # Custom tooltip
                     marker=dict(size=8, color=color),  # Same color for points as the line
                 )
             )
@@ -296,15 +188,12 @@ def build_plot(innovation_df, innovation_summary_df) -> go.Figure:
                     name=code,  # Legend label
                     showlegend=False,
                     line=dict(color=color, width=2),
-                    hovertemplate=f"""{code} {metadata['Indicator Name'][code]} <br>{code} (Metric {innovation_df[innovation_df["Indicator Number"] == code]["Metric"].unique().tolist()}) <br>Year=%{{x:.0f}}<br>Value=%{{y:.2f}}<br>Dt={Dt:.0f} t0={t0:.0f} K={K:.2f}<extra></extra>""",  # Custom tooltip
+                    hovertemplate=f"""{code} <br>{code} (Metric {innovation_df[innovation_df["Indicator Number"] == code]["Metric"].unique().tolist()}) <br>Year=%{{x:.0f}}<br>Value=%{{y:.2f}}<br>Dt={Dt:.0f} t0={t0:.0f} K={K:.2f}<extra></extra>""",  # Custom tooltip
                 )
             )
 
             fig.update_layout(
-                title="Innovation "
-                + selected_innovation
-                + " in "
-                + rev_dict(metadata["Spatial Scale"], selected_spatial_scale_code),
+                title="Innovation " + selected_innovation + " in " + selected_spatial_scale,
                 xaxis_title="Year",
                 yaxis_title="K-normalized value",
                 # hovermode='x unified'
@@ -322,7 +211,7 @@ def build_plot(innovation_df, innovation_summary_df) -> go.Figure:
             fig.add_annotation(
                 x=x_centroid,
                 y=y_centroid,
-                text=f"""{code} {metadata['Indicator Name'][code]}""",
+                text=str(code),
                 showarrow=False,
                 xanchor="center",
                 yanchor="middle",
@@ -341,4 +230,4 @@ fig = build_plot(
     innovation_df=innovation_df_for_plotting,
     innovation_summary_df=innovation_summary_df,
 )
-st.plotly_chart(fig, use_container_width=True)
+st.plotly_chart(fig, width='stretch')
